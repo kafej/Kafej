d05abe7efef36b5b5e1db1c890b6994e1797f908
Michael <kafej666@gmail.com>
2013-12-02 03:48:01 +0100

    My current HomePage

---
+++ js/init.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $('.navbar-nav a').click(function(){
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 500);
+      return false;
+  });
+  $('#languages').popover({trigger:'hover',content:'I made this wallpaper !!',placement:'auto'});
+   // cache the window object
+  $window = $(window);
+ 
+  $('section[data-type="background"]').each(function(){
+    // declare the variable to affect the defined data-type
+    var $scroll = $(this);
+                     
+    $(window).scroll(function() {
+      // HTML5 proves useful for helping with creating JS functions!
+      // also, negative value because we're scrolling upwards                            
+      var yPos = -($window.scrollTop() / $scroll.data('speed'));
+         
+      // background position
+      var coords = '50% '+ yPos + 'px';
+ 
+      // move the background
+       $scroll.css({ backgroundPosition: coords });   
+    }); // end window scroll
+  });  // end section function
+}); // close out script
+
+/* Create HTML5 element for IE */
+document.createElement("section");
